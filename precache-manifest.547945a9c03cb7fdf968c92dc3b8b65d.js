277c5ace65c0167ecaec6702afaf24c14b85492c
ShalomSalaam <110188482+ShalomSalaam@users.noreply.github.com>
2022-07-31 09:51:21 -0400

    Updates

---
+++ precache-manifest.547945a9c03cb7fdf968c92dc3b8b65d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52b896963a712e4c23bc95bbecbc1de0",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "20a6255d0a5364fb9f92",
+    "url": "/calculator/static/css/main.6fdcdd19.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "20a6255d0a5364fb9f92",
+    "url": "/calculator/static/js/main.2042c525.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
